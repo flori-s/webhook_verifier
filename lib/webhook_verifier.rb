bfd369f31254a0e1138f084f768058038ba123d5
flori-s <150121780+flori-s@users.noreply.github.com>
2025-04-28 14:43:45 +0200

    add configuration management to webhook_verifier

--- lib/webhook_verifier.rb
+++ lib/webhook_verifier.rb
@@ -2,3 +2,10 @@
 require 'webhook_verifier/verifier'
 require 'webhook_verifier/errors'
 require 'webhook_verifier/configuration'
+
+
+module WebhookVerifier
+  class << self
+    attr_accessor :configuration
+  end
+end
