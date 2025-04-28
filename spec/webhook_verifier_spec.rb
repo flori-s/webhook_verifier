73eb44e8e80c64f004b040bedeb4c63a5d02ad90
flori-s <150121780+flori-s@users.noreply.github.com>
2025-04-28 11:54:12 +0200

    gem setup

---
+++ spec/webhook_verifier_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WebhookVerifier do
+  it "has a version number" do
+    expect(WebhookVerifier::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
